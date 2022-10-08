9a61e0bd1da68aad9fb1069ed258cbb18b7f93fa
Fede Antonelli <antonelli.fd@gmail.com>
2022-10-08 12:41:15 -0300

    fix: responsive issues

---
+++ src/components/Formation/CoursesList.tsx
@@ -0,0 +1,17 @@
+import { courses } from './data';
+import Course from './Course';
+import styles from './formation.module.scss';
+
+const CoursesList = () => {
+  return (
+    <table className={styles.table}>
+      <tbody>
+        {courses.map((course) => (
+          <Course key={course.id} data={course} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default CoursesList;
